8e6800d1fa44019d4f787d6bd649ca4bd6f8c0df
djaras <duvan318@gmail.com>
2020-09-15 17:33:34 -0500

    Challenge solved. Hidden message discovered

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 # Resolve the problem!!
+import re
 
 
 def run():
     # Start coding here
+    pattern = re.compile(r'[a-zA-Z]+')
+
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            response = re.findall(pattern, line)
+    
+    hide_message = ''.join(response)
+    print(hide_message)
 
 
 if __name__ == '__main__':
